af29a82d2f60293beb09b6f510554a1cf8e54f75
thuynano <thuyhu9876@gmail.com>
2020-11-19 18:13:04 +0700

    add animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,3 +16,13 @@ $(function () {
         $('.search-form').toggleClass('d-none');
     })
 });
+
+
+wow = new WOW({
+    animateClass: 'animated',
+    offset: 100,
+    callback: function (box) {
+        console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+    }
+});
+wow.init();
