29ef8021415bc888ec4635f0c3f08ce5598eb667
thuynano <thuyhu9876@gmail.com>
2020-11-19 09:37:49 +0700

    header

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+$(function () {
+    $('.head-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        nav: false,
+        dots: true,
+        responsive: {
+            
+        }
+    })
+});
