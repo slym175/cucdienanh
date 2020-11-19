02395bc4c70c16979b9ce4533e4349ab7fa2260d
thuynano <thuyhu9876@gmail.com>
2020-11-19 13:32:01 +0700

    header, footer

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,14 @@ $(function () {
         items: 1,
         nav: false,
         dots: true,
+        autoHeight: true,
         responsive: {
             
         }
+    })
+
+    $('.btn-search').click(function (e) {
+        e.preventDefault();
+        $('.search-form').toggleClass('d-none');
     })
 });
