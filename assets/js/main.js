2dfce412bc2ec2d420e9f139f917c3bc3e21c328
thuynano <thuyhu9876@gmail.com>
2020-11-20 17:13:16 +0700

    complete 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,19 @@ $(function () {
         loop: true,
         margin: 0,
         items: 1,
-        nav: false,
+        nav: true,
+        dots: true,
+        autoHeight: true,
+        responsive: {
+            
+        }
+    })
+
+    $('.newest-news-carousel').owlCarousel({
+        loop: true,
+        margin: 0,
+        items: 1,
+        nav: true,
         dots: true,
         autoHeight: true,
         responsive: {
@@ -14,6 +26,23 @@ $(function () {
     $('.btn-search').click(function (e) {
         e.preventDefault();
         $('.search-form').toggleClass('d-none');
+    })
+
+    $('.general-news-row').owlCarousel({
+        loop: true,
+        margin: 16,
+        items: 1,
+        nav: true,
+        dots: false,
+        autoHeight: true,
+        responsive: {
+            768: {
+                items: 2
+            },
+            1200: {
+                items: 3
+            }
+        }
     })
 });
 
